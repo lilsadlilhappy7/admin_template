80b21a91afae58836280d78c757d770a354707be
Thuch Sayi <thouchsayi18@gmail.com>
2026-03-18 21:10:12 +0700

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-
+import { Button } from "@/components/ui/button"
 
 
 export default function Page() {
-  return <h1 className="text-3xl text-red-500 text-center font-bold underline">Hello, Next.js!</h1>
+  return (
+    <div>
+      <Button>Click me</Button>
+      <h1 className="text-3xl text-red-500 text-center font-bold underline">Hello, Next.js!</h1>
+       
+    </div>
+  )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {},
+   theme: {
+    extend: {
+      colors: {
+        border: "oklch(var(--border))",
+        input: "oklch(var(--input))",
+        ring: "oklch(var(--ring))",
+        background: "oklch(var(--background))",
+        foreground: "oklch(var(--foreground))",
+        primary: "oklch(var(--primary))",
+        secondary: "oklch(var(--secondary))",
+        destructive: "oklch(var(--destructive))",
+        muted: "oklch(var(--muted))",
+        accent: "oklch(var(--accent))",
+        popover: "oklch(var(--popover))",
+        card: "oklch(var(--card))",
+      },
+    },
   },
   plugins: [],
 }
